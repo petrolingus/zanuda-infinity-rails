549ef47143e07e16e35c9edfa9970ca2f545f68b
petrolingus <petrolingus@gmail.com>
2025-03-13 03:28:45 +0500

    feat(zir-2): add new command template
    closes issue #2

--- src/main/java/me/petrolingus/zanudainfinityrails/ZanudaInfinityRails.java
+++ src/main/java/me/petrolingus/zanudainfinityrails/ZanudaInfinityRails.java
@@ -1,5 +1,6 @@
 package me.petrolingus.zanudainfinityrails;
 
+import me.petrolingus.zanudainfinityrails.command.MinecartCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,6 +9,7 @@ public class ZanudaInfinityRails extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new MinecartListener(), this);
+        getCommand("minecart").setExecutor( new MinecartCommand());
     }
 
     @Override
@@ -18,5 +20,4 @@ public class ZanudaInfinityRails extends JavaPlugin {
     public static ZanudaInfinityRails getInstance() {
         return getPlugin(ZanudaInfinityRails.class);
     }
-
 }
